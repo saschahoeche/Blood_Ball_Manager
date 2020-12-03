75d74b9d53af799ac85739d3bfe9d4e141348bf4
saschahoeche <sascha.hoeche@gmx.com>
2020-12-03 08:13:41 +0100

    codebase

--- src/main.py
+++ src/main.py
@@ -1,3 +1,45 @@
 # main file
-
 import pygame
+import sys
+
+# constants go here
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 600
+
+pygame.init()
+
+# create window
+gameWindow = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Blood Ball Manager")
+
+
+### GAME LOOP START ###
+gameRunning = True
+
+while gameRunning:
+    
+    ### EVENT LOOP ###
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            gameRunning = False
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print("UP")            
+            if event.key == pygame.K_a:
+                print("LEFT")            
+            if event.key == pygame.K_s:
+                print("DOWN")            
+            if event.key == pygame.K_d:
+                print("RIGHT")
+            
+    ### GAME CONTENT ###
+    gameWindow.fill((255, 255, 255))
+    
+    pygame.display.update()
+    
+
+# close all modules and return memory
+pygame.quit()
+sys.exit()
